effcf14ec914030002846d9ada2cb8316712d371
Hidaya-Bamba <bambahidaya320@gmail.com>
2023-02-16 12:40:41 -0800

    Task-5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is a function that prints a sentence using printf
+ *
+ * Return: 0 if program is a success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+
+}
