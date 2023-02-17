a7db56a1d7711797bf75b2859e0e3f32925c29a9
Hidaya-Bamba <bambahidaya320@gmail.com>
2023-02-17 13:47:31 -0800

    Task-5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all single digit numbers
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+
+	return (0);
+}
