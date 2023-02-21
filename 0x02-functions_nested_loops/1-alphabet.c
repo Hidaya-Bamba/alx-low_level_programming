7784f69395ee0d800f5f720c12e94af3d3dbcda8
Hidaya-Bamba <bambahidaya320@gmail.com>
2023-02-21 04:47:32 -0800

    Task-1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,15 +5,17 @@
  *
  * Description: A function to print alpahbet in lowercase
  *
- * Return: Alwyas zero
+ * Return: Always 0
  */
 
 void print_alphabet(void)
 {
+
 	char i;
 
 	for (i = 'a' ; i <= 'z' ; i++)
 		_putchar(i);
 	_putchar('\n');
+
 }
 
