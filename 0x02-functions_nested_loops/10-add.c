766b5d3086fe19395131edd8e6a8db998fb50bc4
Hidaya-Bamba <bambahidaya320@gmail.com>
2023-02-22 04:08:28 -0800

    Task-10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add -Function to add two numbers
+ *
+ * @a: Parameer 1
+ * @b: Parameter 2
+ *
+ * Return: Sum of two numbers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
