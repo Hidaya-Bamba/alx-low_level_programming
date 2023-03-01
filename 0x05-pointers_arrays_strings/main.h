fe81e0d57f7ab869f13a814af559a65e22f40ca2
Hidaya-Bamba <bambahidaya320@gmail.com>
2023-03-01 09:49:08 -0800

    Task-5

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -14,5 +14,6 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 int _strlen(char *s);
 void _puts(char *str);
- void print_rev(char *s);
+void print_rev(char *s);
+void rev_string(char *s);
 #endif
