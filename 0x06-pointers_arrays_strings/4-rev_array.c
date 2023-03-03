9a88af7892a83c74499dbfd9efbfba7c61305c5f
Hidaya-Bamba <bambahidaya320@gmail.com>
2023-03-03 08:56:10 -0800

    Task-4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * reverse_array - Reverses the content of an array of integers
+ *
+ * @a: Pointer to the first element of the array
+ * @n: Number of elements in the array
+ */
+void reverse_array(int *a, int n)
+{
+	int temp;
+	int *start = a;
+	int *end = a + n - 1;
+
+	while (start < end)
+	{
+	temp = *start;
+	*start = *end;
+	*end = temp;
+
+	start++;
+	end--;
+	}
+}
+
